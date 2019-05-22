720a631e820c7a04c0bf3abf2b15bfd6cf2b44e9
Nan Zhang <znan0419@vt.edu>
2019-05-22 13:18:31 -0400

    update

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 
 @Injectable()
 export class ProductService {
+  private _albumUrl: string ='../assets/album.json';
+  constructor(private _http: Http) { }
 
-  constructor() { }
-
+  getAlbum(id:number):any{
+    return this._http.get(this._albumUrl).map(
+      (response)=>response.json()
+    )
+  }
 }
