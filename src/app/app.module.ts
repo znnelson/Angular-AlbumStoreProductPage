eb9cef94b0f858292bd28a046d33d777197a5993
Nan Zhang <znan0419@vt.edu>
2019-05-22 17:10:22 -0400

    u

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 const appRoutes:Routes=[
   { path:'products', component:ProductListComponent},
   { path:'product/:id', component:ProductPageComponent}
+  { path:'', redirectTo:'products', pathMatch:'full'}
 ];
 
 @NgModule({
