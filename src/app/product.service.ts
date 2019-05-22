4ba26e1bd4780669462051666fc847edf7104111
Nan Zhang <znan0419@vt.edu>
2019-05-22 14:51:41 -0400

    update

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
 
 
 @Injectable()
@@ -8,9 +10,9 @@ export class ProductService {
   private _albumUrl: string ='../assets/album.json';
   constructor(private _http: Http) { }
 
-  getAlbum(id:number){
+  getAlbum(id:number):Observable<Album>{
     return this._http.get(this._albumUrl).map(
-      (response)=>response.json()
+      (response)=><Album>response.json()
     )
   }
 }
