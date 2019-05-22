c8cb84634ebce981a98bfa7e95003a905b6f23d6
Nan Zhang <znan0419@vt.edu>
2019-05-22 13:23:27 -0400

    u

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,7 +8,7 @@ export class ProductService {
   private _albumUrl: string ='../assets/album.json';
   constructor(private _http: Http) { }
 
-  getAlbum(id:number):any{
+  getAlbum(id:number){
     return this._http.get(this._albumUrl).map(
       (response)=>response.json()
     )
